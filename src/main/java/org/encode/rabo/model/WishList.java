b82dee39a6da41deee17d19c4390a20f5e4aed58
Karagwa <annkaragwa@gmail.com>
2024-06-30 15:23:35 +0300

    Created a model class for the wishlists

--- src/main/java/org/encode/rabo/model/WishList.java
+++ src/main/java/org/encode/rabo/model/WishList.java
@@ -1,2 +1,22 @@
-package org.encode.rabo.model;public class WishList {
+package org.encode.rabo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Document
+public class WishList {
+    @Id
+    private String giftListId;
+    @DBRef
+    private String userId;
+    private String occasion;
+    private List<String> gifts;
+
 }

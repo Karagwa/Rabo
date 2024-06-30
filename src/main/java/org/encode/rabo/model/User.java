26f8e0c5dcad687896eb63953c28c4c3ac74b5ed
Karagwa <annkaragwa@gmail.com>
2024-06-30 15:22:54 +0300

    Created a model class for the users

--- src/main/java/org/encode/rabo/model/User.java
+++ src/main/java/org/encode/rabo/model/User.java
@@ -1,2 +1,18 @@
-package org.encode.rabo.model;public class User {
+package org.encode.rabo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document
+public class User {
+    @Id
+    private String userId;
+    private String userName;
+    private String email;
+    private String password;
+
 }

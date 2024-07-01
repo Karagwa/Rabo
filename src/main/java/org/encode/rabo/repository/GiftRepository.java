3defc29a58bc514dd0fc8447c1d9295f84b16136
Karagwa <annkaragwa@gmail.com>
2024-07-01 13:58:11 +0300

    Changes for july 1st in the service classes added methods

--- src/main/java/org/encode/rabo/repository/GiftRepository.java
+++ src/main/java/org/encode/rabo/repository/GiftRepository.java
@@ -3,5 +3,8 @@ package org.encode.rabo.repository;
 import org.encode.rabo.model.Gift;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+
+
 public interface GiftRepository extends MongoRepository<Gift, String> {
+
 }
--- src/main/java/org/encode/rabo/service/GiftService.java
+++ src/main/java/org/encode/rabo/service/GiftService.java
@@ -1,4 +1,36 @@
 package org.encode.rabo.service;
 
+import lombok.RequiredArgsConstructor;
+import org.encode.rabo.exception.EntityNotFoundException;
+import org.encode.rabo.model.Gift;
+import org.encode.rabo.repository.GiftRepository;
+import org.encode.rabo.repository.UserRepository;
+import org.encode.rabo.repository.WishListRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class GiftService {
+
+    private final GiftRepository giftRepository;
+    private final UserRepository userRepository;
+    private final WishListRepository wishListRepository;
+
+    public Gift readGiftById(String id){
+        Optional<Gift> gift= giftRepository.findById(id);
+        if (gift.isPresent()){
+            return gift.get();
+        }
+        throw new EntityNotFoundException("Can't find any gift under the given ID");
+    }
+
+    public List<Gift> readGifts(){
+        return giftRepository.findAll();
+    }
+
+    public Gift createGift(G)
+
 }
